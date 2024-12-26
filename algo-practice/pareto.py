fbb736e34deabc7d80ce4fc2a9d0508b48fe2a1e
Dynasty744 <jessyang650@gmail.com>
2024-12-26 12:04:48 -0800

    maxArea O(n)

--- algo-practice/pareto.py
+++ algo-practice/pareto.py
@@ -19,12 +19,33 @@ class Solution:
     # height focus on the smaller height
     # because the water would spill over
     # then update max of res as necessary
-    res = 0
+    # res = 0
 
-    for l in range(len(height)):
-      for r in range(l + 1, len(height)):
-        area = (r - l) * min(height[l], height[r])
-        res = max(res, area)
+    # for l in range(len(height)):
+    #   for r in range(l + 1, len(height)):
+    #     area = (r - l) * min(height[l], height[r])
+    #     res = max(res, area)
+    # return res
+
+    # O(n) solution
+    # this involves using 2 pointer technique
+    # maximize width initially by setting left and right on edges
+    # we initialize a result variable
+    # then we move the pointer that has the smaller height
+    # while updating the res, whichever is higher
+    # then end when left and right are equal index
+
+    res = 0
+    left = 0
+    right = len(height) - 1
+
+    while left != right:
+      area = (right - left) * min(height[left], height[right])
+      res = max(area, res)
+      if height[left] < height[right]:
+        left += 1
+      else:
+        right -= 1
     return res
 
   def threeSum(self, nums: List[int]) -> List[List[int]]:
