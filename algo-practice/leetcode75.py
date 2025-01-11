83b147af5826e6a114ec1f425c9af5af07e02660
Dynasty744 <jessyang650@gmail.com>
2025-01-10 20:12:13 -0800

    moveZeroes O(n)

--- algo-practice/leetcode75.py
+++ algo-practice/leetcode75.py
@@ -1,7 +1,25 @@
+from typing import List
+
 word1 = "ab"
 word2 = "pqrs"
+nums = [0,1,0,3,12]
 
 class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+      # O(n) using 2 pointers
+      # instead of moving 0s to the right
+      # focus on moving non 0s to the left
+      # left and right will start at the beginning of the list
+      # iterate through the list, whenever right pointer is a non 0
+      # swap the value with the one on left pointer
+      # then immediately increment left pointer
+      left = 0
+      for right in range(len(nums)):
+        if nums[right]:
+          nums[left], nums[right] = nums[right], nums[left]
+          left += 1
+      return nums
+      
     def mergeAlternately(self, word1: str, word2: str) -> str:
       # O(n)
       # setup 2 variables to hold the length of the 2 strings
@@ -29,5 +47,6 @@ class Solution:
       return ''.join(res)    
 
 solution = Solution()
-result = solution.mergeAlternately(word1, word2)
+result = solution.moveZeroes(nums)
+# result = solution.mergeAlternately(word1, word2)
 print(result)
