821fa8383e2092011556be64329a86c11a361912
Dynasty744 <jessyang650@gmail.com>
2025-01-15 15:54:38 -0800

    reverseVowels O(n)

--- algo-practice/leetcode75.py
+++ algo-practice/leetcode75.py
@@ -1,16 +1,45 @@
 from typing import List
 
-word1 = "ab"
-word2 = "pqrs"
-s = "abc"
-t = "acbgdc"
-flowerbed = [1,0,0,0,1]
-n = 1
-nums = [3,1,3,4,3]
-k = 5
+# word1 = "ab"
+# word2 = "pqrs"
+s = "IceCreAm"
+# t = "acbgdc"
+# flowerbed = [1,0,0,0,1]
+# n = 1
+# nums = [3,1,3,4,3]
+# k = 5
 # nums = [0,1,0,3,12]
 
 class Solution:
+  def reverseVowels(self, s: str) -> str:
+    # O(n) 2 pointer approach
+    # we'll use a set() for vowels for O(1) look up
+    # convert the string to a list()
+    # set pointers on both ends of the string
+    # while left is lesser than right
+    # and while left or right is not in vowels
+    # move the pointer inwards
+    # this enables iterations to happen until both are at vowels
+    # if both are vowels, then we use tuple unpacking / simultaneous assignment
+    # make sure to return by joining()
+    vowels = set('aeiouAEIOU')
+    s = list(s)
+    left = 0
+    right = len(s) - 1
+
+    while left < right:
+      while left < right and s[left] not in vowels:
+        left += 1
+      while left < right and s[right] not in vowels:
+        right -= 1
+      
+      if s[left] in vowels and s[right] in vowels:
+        s[left], s[right] = s[right], s[left]
+        right -= 1
+        left += 1
+    
+    return ''.join(s)
+    
   def maxOperations(self, nums: List[int], k: int) -> int:
     # O(n) 2 pointer solution
     # we want to sort nums list to take advantage of the 2 pointer technique
@@ -114,7 +143,8 @@ class Solution:
     return ''.join(res)    
 
 solution = Solution()
-result = solution.maxOperations(nums, k)
+result = solution.reverseVowels(s)
+# result = solution.maxOperations(nums, k)
 # result = solution.canPlaceFlowers(flowerbed, n)
 # result = solution.isSubsequence(s, t)
 # result = solution.moveZeroes(nums)
