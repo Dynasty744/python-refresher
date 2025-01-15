563db4ec7eaca1899ebb9b61e2bb13a09ea2c34f
Dynasty744 <jessyang650@gmail.com>
2025-01-14 17:35:12 -0800

    canPlaceFlowers O(n)

--- algo-practice/leetcode75.py
+++ algo-practice/leetcode75.py
@@ -4,69 +4,89 @@ word1 = "ab"
 word2 = "pqrs"
 s = "abc"
 t = "acbgdc"
+flowerbed = [1,0,0,0,1]
+n = 1
 nums = [0,1,0,3,12]
 
 class Solution:
-    def isSubsequence(self, s: str, t: str) -> bool:
-      # O(n) 2 pointers
-      # we're checking if 's' is a substring of 't'
-      # start by setting pointers 'i' and 'j' at 0 for the 2 strings
-      # while both pointers are lesser than the len of the 2 strings
-      # we only want to move pointer 's' if there's a match with 'j'
-      # we increment 'j' in both cases, so we do that outside of if statement
-      # then we return true if pointer 'i' is equal to length of 's'
-      # because that means we've gotten to the end of the string
-      # and there's a full match of the substring
-      i, j = 0, 0
-      while i < len(s) and j < len(t):
-        if s[i] == t[j]:
-          i += 1
-        j += 1
-      return True if i == len(s) else False
+  def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+    # O(n)
+    # start by placing 0s on both sides of the flowerbed
+    # then start iterate through by skipping the first and last index
+    # for each iteration check that all 3 positions are 0s
+    # if yes, then we place a 1
+    # and decrement n by 1
+    # return is n equal or less than 0
+    f = [0] + flowerbed + [0]
 
-    def moveZeroes(self, nums: List[int]) -> None:
-      # O(n) using 2 pointers
-      # instead of moving 0s to the right
-      # focus on moving non 0s to the left
-      # left and right will start at the beginning of the list
-      # iterate through the list, whenever right pointer is a non 0
-      # swap the value with the one on left pointer
-      # then immediately increment left pointer
-      left = 0
-      for right in range(len(nums)):
-        if nums[right]:
-          nums[left], nums[right] = nums[right], nums[left]
-          left += 1
-      return nums
-      
-    def mergeAlternately(self, word1: str, word2: str) -> str:
-      # O(n)
-      # setup 2 variables to hold the length of the 2 strings
-      # setup 2 pointers to use as indexes for the 2 strings
-      # while the index of either string is smaller than length of either string
-      # if index of first string is < than length of first string
-      # then add the indexed char to res
-      # then increment the index
-      # perform same steps for the second string
-      # return result
-      l1 = len(word1)
-      l2 = len(word2)
-      p1 = 0
-      p2 = 0
-      res = []
+    for i in range(1, len(f) - 1):
+      if f[i - 1] == 0 and f[i] == 0 and f[i + 1] == 0:
+        f[i] = 1
+        n -= 1
+    
+    return n <= 0
 
-      while p1 < l1 or p2 < l2:
-        if p1 < l1:
-          res += word1[p1]
-          p1 += 1
-        if p2 < l2:
-          res += word2[p2]
-          p2 += 1
-          
-      return ''.join(res)    
+  def isSubsequence(self, s: str, t: str) -> bool:
+    # O(n) 2 pointers
+    # we're checking if 's' is a substring of 't'
+    # start by setting pointers 'i' and 'j' at 0 for the 2 strings
+    # while both pointers are lesser than the len of the 2 strings
+    # we only want to move pointer 's' if there's a match with 'j'
+    # we increment 'j' in both cases, so we do that outside of if statement
+    # then we return true if pointer 'i' is equal to length of 's'
+    # because that means we've gotten to the end of the string
+    # and there's a full match of the substring
+    i, j = 0, 0
+    while i < len(s) and j < len(t):
+      if s[i] == t[j]:
+        i += 1
+      j += 1
+    return True if i == len(s) else False
+
+  def moveZeroes(self, nums: List[int]) -> None:
+    # O(n) using 2 pointers
+    # instead of moving 0s to the right
+    # focus on moving non 0s to the left
+    # left and right will start at the beginning of the list
+    # iterate through the list, whenever right pointer is a non 0
+    # swap the value with the one on left pointer
+    # then immediately increment left pointer
+    left = 0
+    for right in range(len(nums)):
+      if nums[right]:
+        nums[left], nums[right] = nums[right], nums[left]
+        left += 1
+    return nums
+    
+  def mergeAlternately(self, word1: str, word2: str) -> str:
+    # O(n)
+    # setup 2 variables to hold the length of the 2 strings
+    # setup 2 pointers to use as indexes for the 2 strings
+    # while the index of either string is smaller than length of either string
+    # if index of first string is < than length of first string
+    # then add the indexed char to res
+    # then increment the index
+    # perform same steps for the second string
+    # return result
+    l1 = len(word1)
+    l2 = len(word2)
+    p1 = 0
+    p2 = 0
+    res = []
+
+    while p1 < l1 or p2 < l2:
+      if p1 < l1:
+        res += word1[p1]
+        p1 += 1
+      if p2 < l2:
+        res += word2[p2]
+        p2 += 1
+        
+    return ''.join(res)    
 
 solution = Solution()
-result = solution.isSubsequence(s, t)
+result = solution.canPlaceFlowers(flowerbed, n)
+# result = solution.isSubsequence(s, t)
 # result = solution.moveZeroes(nums)
 # result = solution.mergeAlternately(word1, word2)
 print(result)
